462a6a6797a1f8095b05801e5b32335803fb82ea
Muhammad Ali <this.is.muhammad.ali@gmail.com>
2018-04-01 20:36:36 -0500

    Added navbar

--- cxe_webclient/src/App.js
+++ cxe_webclient/src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import CXENavBar from './Components/CXENavBar';
+import { BrowserRouter as Router } from 'react-router-dom'
+import RightBar from './Components/RightBar';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <CXENavBar>
+          </CXENavBar>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">CXE Monitor</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </Router>
     );
   }
 }
---
+++ cxe_webclient/src/Components/CXENavBar.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Nav,Navbar, NavItem, Button, MenuItem, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
+class CXENavBar extends Component {
+  render() {
+    return (
+      <Navbar inverse>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to={'/home'}>CXE Monitor</Link>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <NavItem eventKey={1} href="index">
+            Overview
+          </NavItem>
+        </Nav>
+      </Navbar>
+    )
+  }
+}
+
+export default CXENavBar;
